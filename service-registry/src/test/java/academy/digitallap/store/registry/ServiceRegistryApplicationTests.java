ff5211b4d4710269eac2a039101544a63978715a
joosbeen <joosbeen@gmail.com>
2021-03-22 11:39:58 -0600

    config de service-registry de eureka

---
+++ service-registry/src/test/java/academy/digitallap/store/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitallap.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
