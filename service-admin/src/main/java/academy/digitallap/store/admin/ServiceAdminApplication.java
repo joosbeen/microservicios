f6a055cc1690925d39182f43afde39d12a9227c5
joosbeen <joosbeen@gmail.com>
2021-03-28 07:38:28 -0600

    Final del curso

---
+++ service-admin/src/main/java/academy/digitallap/store/admin/ServiceAdminApplication.java
@@ -0,0 +1,16 @@
+package academy.digitallap.store.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class ServiceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAdminApplication.class, args);
+	}
+
+}
---
+++ service-admin/src/test/java/academy/digitallap/store/admin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitallap.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
